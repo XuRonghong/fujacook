d91204748d79467e04ae231ca116d08c2bfc6e5f
ronghong <ronghong@fujacook.com>
2019-06-26 11:36:23 +0800

    test connect sqlsrv.php + .bak

---
+++ public/test2.php
@@ -0,0 +1,17 @@
+<?
+echo phpinfo();
+
+$dsn = 'sqlsrv:server=127.0.0.1;Database=fujacook;';
+$user = 'fujacook';
+$password = '3Tv8u8LcrQQBl7X1';
+ 
+// 建立 PDO 物件
+$pdo = new PDO($dsn, $user, $password);
+ 
+$sql = "SELECT * FROM testtable";
+$pre = $pdo->prepare($sql);
+$pre->execute();
+$row = $pre->fetchAll(2);
+ 
+print_r($row);
+?>
