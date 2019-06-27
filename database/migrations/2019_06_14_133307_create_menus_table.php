76f9b7f63f4e7f2e5cd35f0d027fca1911411ee4
ronghong <hello080810@gmail.com>
2019-06-27 13:38:20 +0800

    add DB_refresh to migrate

--- database/migrations/2019_06_14_133307_create_menus_table.php
+++ database/migrations/2019_06_14_133307_create_menus_table.php
@@ -14,26 +14,25 @@ class CreateMenusTable extends Migration
     public function up()
     {
         if (env('DB_REFRESH')) {
-        }
-        Schema::create('menus', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('parent_id')->comment('父類別')->default(0);
-            $table->integer('rank')->comment('序')->default(0);
-            $table->string('type')->comment('類')->nullable();
-            $table->string('name')->comment('title')->default('未命名標題');
-            $table->string('link')->comment('連結')->default('');
-            $table->tinyInteger('sub_menu')->comment('是否有子類別')->default(1);
-            $table->string('access')->comment('')->nullable();
-            $table->tinyInteger('open')->comment('開放')->default(1);
-            $table->timestamps();
-        });
+            Schema::create('menus', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('parent_id')->comment('父類別')->default(0);
+                $table->integer('rank')->comment('序')->default(0);
+                $table->string('type')->comment('類')->nullable();
+                $table->string('name')->comment('title')->default('未命名標題');
+                $table->string('link')->comment('連結')->default('');
+                $table->tinyInteger('sub_menu')->comment('是否有子類別')->default(1);
+                $table->string('access')->comment('')->nullable();
+                $table->tinyInteger('open')->comment('開放')->default(1);
+                $table->timestamps();
+            });
 
-        Schema::create('admin_menu', function (Blueprint $table) {
-            $table->increments('id')->comment('流水編號');
-            $table->unsignedInteger('admin_id')->comment('對應admin.id');
-            $table->unsignedInteger('menu_id')->comment('對應menus.id');
-            $table->tinyInteger('open')->comment('開放')->default(1);
-            $table->timestamps();
+            Schema::create('admin_menu', function (Blueprint $table) {
+                $table->increments('id')->comment('流水編號');
+                $table->unsignedInteger('admin_id')->comment('對應admin.id');
+                $table->unsignedInteger('menu_id')->comment('對應menus.id');
+                $table->tinyInteger('open')->comment('開放')->default(1);
+                $table->timestamps();
 
 //            $table->morphs('model');
 
@@ -42,7 +41,8 @@ class CreateMenusTable extends Migration
 //                ->on($table_names['permissions'])
 //                ->onDelete('cascade');
 
-        });
+            });
+        }
     }
 
     /**
@@ -53,8 +53,8 @@ class CreateMenusTable extends Migration
     public function down()
     {
         if (env('DB_REFRESH')) {
+            Schema::dropIfExists('admin_menu');
+            Schema::dropIfExists('menus');
         }
-        Schema::dropIfExists('admin_menu');
-        Schema::dropIfExists('menus');
     }
 }
